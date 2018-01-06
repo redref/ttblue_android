5ed7aaa3f42ea70c05b4602f83fbfa2ecb35ccfc
redref <ref.redcoder@gmail.com>
2018-01-06 15:50:52 +0100

    Add ttwatch/ttbincnv as a dep

--- app/src/main/java/ttblue_android/com/ttblue_android/MainActivity.java
+++ app/src/main/java/ttblue_android/com/ttblue_android/MainActivity.java
@@ -33,11 +33,6 @@ public class MainActivity extends Activity {
     /* Tools */
     private SingBroadcastReceiver mReceiver;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,12 +90,6 @@ public class MainActivity extends Activity {
             }
         }
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 
     private class SingBroadcastReceiver extends BroadcastReceiver {
         public void onReceive(Context context, Intent intent) {
